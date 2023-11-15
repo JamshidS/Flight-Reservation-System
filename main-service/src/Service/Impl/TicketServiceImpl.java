063488f774344c6b60d7221ecdc6c14494989246
eycwave <emreyuce000@gmail.com>
2023-11-15 13:19:00 +0300

    [BA-7] - Some updates

--- main-service/src/Service/Impl/TicketServiceImpl.java
+++ main-service/src/Service/Impl/TicketServiceImpl.java
@@ -7,13 +7,36 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class TicketServiceImpl implements TicketService {
-
     private final List<Ticket> ticketList = new ArrayList<>();
 
     @Override
     public void createTicket(Ticket ticket) {
         ticketList.add(ticket);
-        System.out.println("Ticket has been created with ticket no: " + ticket.getTicketNo());
+        System.out.println("Ticket has been created. Ticket no: " + ticket.getTicketNo());
+    }
+
+    @Override
+    public void updateTicket(Ticket ticket) {
+        // İşlemler gelecek...
+        System.out.println("Ticket has been updated. Ticket no: " + ticket.getTicketNo());
+    }
+
+    @Override
+    public String cancelTicket(long ticketNo, long userID) {
+        String response = "Ticket not found. Unable to cancel.";
+        for(Ticket ticket: ticketList) {
+            if (ticket.getTicketNo() == ticketNo) {
+                ticketList.remove(ticket);
+                response = "Ticket with ticket no " + ticketNo + " has been canceled successfully.";
+            }
+        }
+        return response;
+    }
+
+    @Override
+    public void purchaseTicket(Ticket ticket) {
+        // İşlemler gelecek...
+        System.out.println("You have successfully purchased ticket. Your ticket no is: " + ticket.getTicketNo());
     }
 
     @Override
@@ -27,20 +50,13 @@ public class TicketServiceImpl implements TicketService {
     }
 
     @Override
-    public List<Ticket> getAllTickets() {
-        return ticketList;
+    public List<Ticket> getTicketByUserID(int userID) {
+        // İşlemler gelecek...
+        return null;
     }
 
     @Override
-    public String cancelTicket(long ticketNo) {
-        String response = "Ticket not found. Unable to cancel.";
-        for(Ticket ticket: ticketList) {
-            if (ticket.getTicketNo() == ticketNo) {
-                ticketList.remove(ticket);
-                response = "Ticket with ticket no " + ticketNo + " has been canceled successfully.";
-            }
-        }
-        return response;
+    public List<Ticket> getAllTickets() {
+        return ticketList;
     }
-
 }
--- main-service/src/Service/TicketService.java
+++ main-service/src/Service/TicketService.java
@@ -6,8 +6,22 @@ import java.util.List;
 
 public interface TicketService {
     void createTicket(Ticket ticket);
-    Ticket getTicketByTicketNo(long ticketNo);
-    List<Ticket> getAllTickets();
-    String cancelTicket(long ticketNo);
 
+    void updateTicket(Ticket ticket);
+    // Purchase olmadan önce update edilebiliyor olacak.
+    // Örneğin: Bilet fiyatı güncellemesi (Bilet sayısı az ise bilet fiyatını artırsın: Toplam biletin %10 u kaldıysa fiyata %30 zam gibi)
+
+    String cancelTicket(long ticketNo, long userID);
+    // Eğer user, bilet'i cancel ettiyse %10 kesintili olacak şekilde iade olacak.
+
+    void purchaseTicket(Ticket ticket);
+    // Ticket almayı netleştireceği bir fonksiyon olmalı.
+    // Buy sonrası hash map ile user tutulabilir. {ticket_no: user_id}
+
+    Ticket getTicketByTicketNo(long ticketNo);
+
+    List<Ticket> getTicketByUserID(int userID);
+    // User'a ait ticket'ları listeleme.
+
+    List<Ticket> getAllTickets();
 }
